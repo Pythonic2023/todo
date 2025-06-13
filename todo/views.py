3ebdaa1bcd441e005183104d819411851bdc1912
Pythonic2023 <124443031+Pythonic2023@users.noreply.github.com>
2025-06-13 07:11:47 -0400

    Sign in function fixed

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, HttpResponseRedirect
 from django.urls import reverse
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, authenticate
 from django.contrib import messages
 
 # Create your views here.
@@ -19,7 +19,8 @@ def signup(request):
 		else:
 			messages.error(request, "Registration failed. Please correct the errors below")
 			print(form.errors)
-			return signuperror(request)
+			error = form.errors
+			return signuperror(request, error)
 	else:
 		form = UserCreationForm()
 	return render(request, "todo/signup.html", {"form": form})
@@ -29,12 +30,22 @@ def signupsuccess(request, user):
 	context = {'username': username}
 	return render(request, "todo/signupsuccess.html", context)
 
-def signuperror(request):
+def signuperror(request, error):
 	error_message = "Sign up failed"
-	context = {'error_message': error_message}
+	context = {'error_message': error_message, 'error': error}
 	return render(request, "todo/signuperror.html", context)
 	
-	
-
 def signin(request):
-	return render(request, "todo/signin.html")
+	if request.method == "POST":
+		form = AuthenticationForm(request.POST)
+		username = request.POST.get("username")
+		password = request.POST.get("password")
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			return redirect('/todo/')
+		else:
+			print("Unable to login")
+	else: 
+		form = AuthenticationForm()
+	return render(request, "todo/signin.html", {'form': form})
